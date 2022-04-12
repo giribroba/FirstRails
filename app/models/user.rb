e84becdfc3b6e2299f2ae84094c9223018c7fa61
giribroba <giribroba@gmail.com>
2022-04-12 15:32:45 -0300

    New user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :age, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 150 }
 end
