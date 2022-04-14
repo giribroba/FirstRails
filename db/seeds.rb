68c970d86b621289e3b16b9889fc208ef1665741
giribroba <giribroba@gmail.com>
2022-04-14 17:07:16 -0300

    Add avatar photo to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
 User.delete_all
 
-7.times do |i|
-  User.create!(name: Faker::Games::ElderScrolls.name, age: rand(15..50))
-end
+require 'uri'
+require 'net/http'
+
+private
+  def get_avatar(name)
+    search = 'skyrim+' + name.split[0]
+    result = JSON.parse(Net::HTTP.get(URI.parse('https://serpapi.com/search.json?q=' + search +'&tbm=isch&ijn=0&api_key=' + Rails.application.credentials.dig(:serpapi_key))))
+    
+    if result['error'].nil?
+      return result['images_results'][0]['thumbnail']
+    else
+      return 'Error: '+result['error']
+    end
+    
+  end
+
+
+  1.times do |i|
+    name = Faker::Games::ElderScrolls.name
+
+    User.create!(
+      name: name, 
+      age: rand(15..50),
+      avatar: get_avatar(name)
+    )
+  end 
